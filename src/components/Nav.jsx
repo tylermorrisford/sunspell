501978dd98def2a685fd4ea69c2fac12c754d87c
Tyler Morris Ford <tylerford@gmail.com>
2021-04-29 16:40:26 -0600

    fade the nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import FadeDiv from './FadeDiv';
 
 const navStyle = {
     fontSize: '1.7rem', 
@@ -16,20 +17,22 @@ const brandStyle = {
 
 const Navigation = () => {
     return(
-    <Navbar collapseOnSelect expand="lg" style={navStyle}>
-        <Navbar.Brand style={{fontSize: '1.7rem'}}>
-            <LinkContainer to="/" style={brandStyle}><Nav.Link to="/">Sunspell Records</Nav.Link></LinkContainer>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto">
-            <LinkContainer style={navStyle} activeStyle={{color: '#a4826c'}} to="/gear"><Nav.Link href="/">Gear</Nav.Link></LinkContainer>  
-            <LinkContainer style={navStyle} activeStyle={{color: '#a4826c'}} to="/services"><Nav.Link href="/">Services</Nav.Link></LinkContainer>  
-            <LinkContainer style={navStyle} activeStyle={{color: '#a4826c'}} to="/contact"><Nav.Link href="/">Contact</Nav.Link></LinkContainer>  
-            <LinkContainer style={navStyle} activeStyle={{color: '#a4826c'}} to="/about"><Nav.Link href="/">About</Nav.Link></LinkContainer>  
-        </Nav>
-    </Navbar.Collapse>
-    </Navbar>
+        <FadeDiv>
+            <Navbar collapseOnSelect expand="lg" style={navStyle}>
+                <Navbar.Brand style={{fontSize: '1.7rem'}}>
+                    <LinkContainer to="/" style={brandStyle}><Nav.Link to="/">Sunspell Records</Nav.Link></LinkContainer>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ml-auto">
+                    <LinkContainer style={navStyle} activeStyle={{color: '#a4826c'}} to="/gear"><Nav.Link href="/">Gear</Nav.Link></LinkContainer>  
+                    <LinkContainer style={navStyle} activeStyle={{color: '#a4826c'}} to="/services"><Nav.Link href="/">Services</Nav.Link></LinkContainer>  
+                    <LinkContainer style={navStyle} activeStyle={{color: '#a4826c'}} to="/contact"><Nav.Link href="/">Contact</Nav.Link></LinkContainer>  
+                    <LinkContainer style={navStyle} activeStyle={{color: '#a4826c'}} to="/about"><Nav.Link href="/">About</Nav.Link></LinkContainer>  
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+        </FadeDiv>
     );
 }
 
