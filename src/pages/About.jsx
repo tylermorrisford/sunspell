f7442bb7d272f718dd7060f8ff89dc478465c344
Tyler Morris Ford <tylerford@gmail.com>
2022-09-11 08:28:13 -0600

    fix line break

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
             <Holder />
         </FadeDiv>
         <PageBody>
-            <h2>Sunspell Records is a Salt Lake City based recording studio located in the University district. Our mission is to provide a friendly and comfortable environment conducive to the artist's needs in order to foster creativity and achieve the artist's vision.</h2>
+            <h2>Sunspell Records is a Salt Lake City based recording studio located in the University district. <br />Our mission is to provide a friendly and comfortable environment conducive to the artist's needs in order to foster creativity and achieve the artist's vision.</h2>
         </PageBody>
         </>
     );
