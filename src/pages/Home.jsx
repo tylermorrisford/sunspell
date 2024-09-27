f857f1764feb70996d93bed5345b42ef77087b0a
Tyler M Ford <tylerford@gmail.com>
2024-09-27 15:54:51 -0600

    minify homepage image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
-import amps from '../images/Sunspell31.jpg'
+import amps from '../images/Sunspell31-min.jpg';
 import '../App.css';
 import FadeDiv from '../components/FadeDiv';
 
-
 export default function Home() {
-
-
-    return(
-        <Holder>
-            <FadeDiv>   
-                <Overlay>SALT LAKE CITY<br />RECORDING SERVICES</Overlay>
-                {/* <SunspellLogo style={{width: '350px', height: '350px'}} className='App-logo' /> */}
-            </FadeDiv> 
-        </Holder>
-    );
-
+  return (
+    <FadeDiv>
+      <Holder>
+        <Overlay>
+          SALT LAKE CITY
+          <br />
+          RECORDING SERVICES
+        </Overlay>
+      </Holder>
+    </FadeDiv>
+  );
 }
 
 const Overlay = styled.h1`
-font-size: 5em;
-font-weight: 900;
-color: white;
-filter: drop-shadow(0 0 0.75rem black);
-`
+  font-size: 5em;
+  font-weight: 900;
+  color: white;
+  filter: drop-shadow(0 0 0.75rem black);
+`;
 
 const Holder = styled.div`
-width: 100%;
-height: 80vh;
-background-image: url(${amps});
-background-size: cover;
-background-position: center;
-background-repeat: no-repeat;
-text-align: center;
-display: flex;
-align-items: center;
-justify-content: center;
-`
-
+  width: 100%;
+  height: 80vh;
+  background-image: url(${amps});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
