4ed6fbe148db3a9d45adbebcae458187ea1bda7f
Tyler M Ford <tylerford@gmail.com>
2024-09-27 16:30:13 -0600

    refactor: optimizer second package fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-// import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite';
-import { imagetools } from 'vite-imagetools'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
@@ -10,16 +9,13 @@ export default defineConfig(() => {
     },
     plugins: [
       react(),
-      imagetools(),
-      // ViteImageOptimizer({
-      //   include: ['src/images/*.jpg', 'src/images/*.png'],
-      //   jpeg: {
-      //     quality: 10,
-      //     progressive: true,
-      //     mozjpeg: true,
-      //   },
-      //   gzip: true,
-      // }),
+      ViteImageOptimizer({
+        jpeg: {
+          quality: 65,
+          progressive: true,
+          mozjpeg: true,
+        },
+      }),
     ],
   };
 });
