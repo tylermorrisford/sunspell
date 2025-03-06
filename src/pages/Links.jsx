c07e1ab15e07c47ebf1534eb653e765cf909612f
Tyler M Ford <tylerford@gmail.com>
2025-03-05 17:57:43 -0700

    fix spotify link

--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import LinksPhoto from '../images/SunSpell_Links.jpg';
 import { PageBody, YouTubePlayer, PhotoFrame } from '../components';
+import spotify from '../images/spotify_logo.png';
 
 const LinksContainer = styled.div`
   display: flex;
@@ -12,23 +13,32 @@ const LinksContainer = styled.div`
   margin: 1rem;
 `;
 
+const PlaylistContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+  margin: 1rem;
+  @media (max-width: 768px) {
+    margin: 0;
+  }
+`;
+
 const Links = () => {
   return (
     <>
       <PhotoFrame photo={LinksPhoto} />
       <PageBody>
         <h2>Links</h2>
-        <ul>
-          <li>
-            <a
-              href='https://open.spotify.com/playlist/0d2zLnAvffKO60MfrDqECo?si=db7e4a6f68004024'
-              target='_blank'
-              rel='noreferrer'
-            >
-              The Sunspell Spotify Playlist
-            </a>
-          </li>
-        </ul>
+        <PlaylistContainer>
+          <a
+            href='https://open.spotify.com/playlist/0d2zLnAvffKO60MfrDqECo?si=db7e4a6f68004024'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src={spotify} alt='Spotify' height='20px' /> The Sunspell
+            Spotify Playlist
+          </a>
+        </PlaylistContainer>
         <LinksContainer>
           <YouTubePlayer youtubeUrl='https://youtu.be/8WlhNseD2H4?feature=shared' />
           <YouTubePlayer youtubeUrl='https://youtu.be/7iEtt-Z8_C4?si=l1zoM966OFR1umFm' />
